63207dc2452bed011149c7768466ecfb30ed91ef
Trung Nguyen <leshin2015@gmail.com>
2018-03-26 02:34:10 +0700

    Implement article area

---
+++ src/components/common/article/article-big.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="article-big col-md-12">
+    <div class="article-big-bg" :style="{ 'background-image': 'url(' + article.picture + ')' }"></div>
+    <div class="article-big-meta">
+      <h5>{{article.title}}</h5>
+      <p class="daytime">{{article.daytime}}</p>
+      <p class="desc" v-if="article.description">{{article.description.substring(0, 300)}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'article-big',
+  props: ['article'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style>
+</style>
